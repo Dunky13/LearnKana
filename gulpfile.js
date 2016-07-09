f870a0696da4552143dbdbe3ee86612618722805
Dunky13 <mwent@live.nl>
2016-07-09 11:28:57 +0200

    Move some functions into an Interface class

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');  
 
-gulp.task('scripts', function() {
+gulp.task('min', function() {
 	return gulp.src(['./js/src/preload.js', './js/src/jchar.js', './js/src/jblock.js', './js/src/q.js', './js/src/jall.js', './js/src/gapi.js', './js/src/index.js'])
 		.pipe(concat('index.js'))
 		.pipe(uglify())
+		.pipe(gulp.dest('./js/dist/'));
+});
+gulp.task('long', function() {
+	return gulp.src(['./js/src/preload.js', './js/src/jchar.js', './js/src/jblock.js', './js/src/q.js', './js/src/jall.js', './js/src/gapi.js', './js/src/index.js'])
+		.pipe(concat('index.js'))
 		.pipe(gulp.dest('./js/dist/'));
 });
--- js/main.js
+++ js/main.js
@@ -27,62 +27,20 @@ require(['code'], function (a) {
 
 	$(document).ready(function(){
 		constants	= {};
+		interface	= new Interface();
 		localStore	= new Local();
-		chars 		= loadCharacters();
+		chars 		= interface.loadCharacters();
 		q 			= undefined;//chars.getQuestion();
 		if(localStore.getJSON()){
 			chars.load();
 		}
-		nextQuestion();
+		interface.nextQuestion();
 
-		$(".A").click(function(){
-			var correct = q.answer($(this).text());
-			if($(this).hasClass("disabled")){return;}
-			if(correct)
-			{
-				$(".A:contains("+correct.romaji+")").toggleClass("label-warning", true);
-				$(".A:contains("+correct.hiragana+")").toggleClass("label-warning", true);
-				$(".A:contains("+correct.katakana+")").toggleClass("label-warning", true);
-				$(".RW .correctness").toggleClass("fa-check",true);
-				$(".RW .correctness").toggleClass("fa-times",false);
-				$(".A").toggleClass("disabled",true);
-				$(this).toggleClass("label-success",true).toggleClass("label-warning",false);
-				$("#next").toggleClass("disabled", false);
-				//setTimeout(function(){
-				//	showNewQuestion();
-				//},1500);
-			}
-			else
-			{
-				$(".RW .correctness").toggleClass("fa-check",false);
-				$(".RW .correctness").toggleClass("fa-times",true);
-				$(this).toggleClass("label-danger",true);
-				$("#next").toggleClass("disabled", true);
-			}
-			updateScore();
-		});
-		$("#next").click(function(){
-			if(!$(this).hasClass("disabled")){
-				nextQuestion()
-			}
-		});
+		$(".A").click(interface.clickAnswer);
+		$("#next").click(interface.clickNext);
 		$(window).on('beforeunload', function(){
 			g.saveToDrive();
 			//return "Are you sure you want to leave?";
 		});
 	});
 });
-
-/* require.config({
-    paths: {
-        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min',
-		bootstrap: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min",
-		msgpack: "//cdnjs.cloudflare.com/ajax/libs/msgpack-lite/0.1.20/msgpack.min",
-		gapi: "//apis.google.com/js/client:platform",
-		code: "index"
-    }
-});
-
-require(["jquery"], function ($) {
-    console.log($.fn.jquery);
-}); */
